c98d546d32b02c48a92b50cf0da3b728d53655cd
shahzaib-1997 <smsashafqat@gmail.com7>
2023-07-31 15:14:00 +0500

    Updated settings.py

--- quiz_project/settings.py
+++ quiz_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,3 +143,5 @@ CHANNEL_LAYERS = {
 #         'BACKEND': 'channels.layers.InMemoryChannelLayer',
 #     },
 # }
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
