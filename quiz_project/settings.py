f9e06b0122b0ba77040381bb616bb2fbf6873818
shahzaib-1997 <smsashafqat@gmail.com7>
2023-07-31 15:02:45 +0500

    Updated settings.py

--- quiz_project/settings.py
+++ quiz_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-t^4)gxbc*qwgwq!l!fsip=k5k-$)*&h+v48_!ci-28dl12f^9n"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost']
 
 
 # Application definition
